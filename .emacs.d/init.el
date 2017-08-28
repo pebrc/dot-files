bc14fe9bacb4e14618f209ae0298bb3d3b07f32b
Peter Brachwitz <peter.brachwitz@elastic.co>
2017-08-28 17:17:03 +0200

    Add scala tools, restclient, json-mode, multiple cursors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -64,6 +64,18 @@
   :if (memq window-system '(mac ns))
   :config (exec-path-from-shell-initialize))
 (use-package markdown-mode)
+(use-package ensime
+  :ensure t
+  :pin melpa-stable)
+
+(use-package scala-mode
+  :interpreter
+  ("scala" . scala-mode))
+
+(use-package multiple-cursors)
+(use-package json-mode)
+(use-package restclient)
+
 ;;
 ;; Customizations
 
@@ -77,9 +89,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ensime-typecheck-idle-interval 1.5)
+ '(linum-format "%4d │")
  '(markdown-command "multimarkdown")
  '(menu-bar-mode nil)
- '(package-selected-packages (quote (ace-jump-mode)))
+ '(package-selected-packages
+   (quote
+    (multiple-cursors restclient projectile popup-imenu ensime json-mode markdown-mode ace-jump-mode)))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
