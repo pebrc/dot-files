25a41205fb22d5b39da2fdaf75d40da0867f96ed
Peter Brachwitz <peter.brachwitz@elastic.co>
2017-09-15 13:46:42 +0200

    Add mac pasteboard integration

--- .emacs.d/lisp/efuncs.el
+++ .emacs.d/lisp/efuncs.el
@@ -52,3 +52,12 @@
     (goto-char (point-max))
     (insert "(figwheel-sidecar.repl-api/cljs-repl)")
     (cider-repl-return)))
+
+(defun mac-cut-function (text &optional rest)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(defun mac-paste-function ()
+  (shell-command-to-string "pbpaste"))
--- .emacs.d/lisp/hooks.el
+++ .emacs.d/lisp/hooks.el
@@ -1,3 +1,7 @@
 (global-company-mode)
 (global-auto-revert-mode t)
 (window-numbering-mode)
+(when (and (string= "darwin" system-type)
+           (not (memq window-system '(mac ns))))
+  (setq interprogram-cut-function 'mac-cut-function)
+  (setq interprogram-paste-function 'mac-paste-function))
